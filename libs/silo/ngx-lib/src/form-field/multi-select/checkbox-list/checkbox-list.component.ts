f7850d931113cfa1e1c7886f7c6b252a7530b6e2
Michael Nguyen <dev.michael.nguyen@gmail.com>
2021-09-20 21:39:15 -0400

    Update checkbox list read-only style

--- libs/silo/ngx-lib/src/form-field/multi-select/checkbox-list/checkbox-list.component.ts
+++ libs/silo/ngx-lib/src/form-field/multi-select/checkbox-list/checkbox-list.component.ts
@@ -15,36 +15,10 @@ import { MultiSelectFieldComponent } from '../multi-select-field.component';
   styleUrls: ['./checkbox-list.component.scss'],
 })
 export class CheckboxListComponent
-  extends MultiSelectFieldComponent
-  implements AfterViewInit {
+  extends MultiSelectFieldComponent {
   @ViewChildren(MatListOption)
   matListOptions: QueryList<MatListOption>;
 
   @Input()
   fieldSize: ClassExpression = 'col-6';
-
-  ngAfterViewInit() {
-    this.setSelectionListAsReadOnly();
-  }
-
-  setSelectionListAsReadOnly() {
-    if (!this.isReadOnly) {
-      return;
-    }
-
-    // TODO: This will work for now until we need to dynamically change from read-only to editable
-    this.matListOptions.forEach((x) => {
-      x.toggle = () => {
-        // do nothing
-      };
-    });
-
-    this._elementRef.nativeElement
-      .querySelectorAll('.mat-list-option')
-      .forEach((item) => {
-        if (item.attributes.getNamedItem('aria-disabled').nodeValue == 'true') {
-          item.removeAttribute('tabindex');
-        }
-      });
-  }
 }
--- libs/silo/ngx-lib/src/form-field/multi-select/checkbox-list/checkbox-list.module.ts
+++ libs/silo/ngx-lib/src/form-field/multi-select/checkbox-list/checkbox-list.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatListModule } from '@angular/material/list';
 import { ResponsiveContainerModule } from '../../../responsive/responsive-container/responsive-container.module';
 import { LabelModule } from '../../label/label.module';
@@ -13,10 +14,11 @@ import { CheckboxListComponent } from './checkbox-list.component';
     LabelModule,
     MatButtonModule,
     MatListModule,
+    MatCheckboxModule,
     ReactiveFormsModule,
     ResponsiveContainerModule,
   ],
   declarations: [CheckboxListComponent],
   exports: [CheckboxListComponent],
 })
-export class CheckboxListModule {}
+export class CheckboxListModule { }
